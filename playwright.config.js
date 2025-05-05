fc92cd1672813f4fd219164ac51e4a32bdd7fefb
Santeri Hanhinen <santeri.hanhinen@gmail.com>
2025-05-05 15:21:10 +0300

    finally playwright works (maybe)

--- playwright.config.js
+++ playwright.config.js
@@ -1,13 +1,13 @@
 import { defineConfig } from '@playwright/test'
 export default defineConfig({
-  testDir: 'test/e2e-tests',
+  testDir: './test/e2e-tests',
   webServer: {
-    command: 'npm run start',
-    url: 'http://localhost:8081',
-    timeout: 120 * 1000,
+    command: 'npm start',
+    url: 'http://localhost:8080',
+    timeout: 10 * 1000,
     reuseExistingServer: !process.env.CI,
   },
   use: {
-    baseURL: 'http://localhost:8081/',
+    baseURL: 'http://localhost:8080/',
   },
 })
