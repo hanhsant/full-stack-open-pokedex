795edb8c6a2e3607019476871529a976aca407df
Santeri Hanhinen <santeri.hanhinen@gmail.com>
2025-05-05 13:21:26 +0300

    linter errror linebreak removed

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,10 +24,7 @@ module.exports = {
       'error',
       2
     ],
-    'linebreak-style': [
-      'error',
-      'windows'
-    ],
+    'linebreak-style': 'off',
     'quotes': [
       'error',
       'single'
