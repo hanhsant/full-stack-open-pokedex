cfa481a33f6c242c90d63ef4d6284418db379abf
Santeri Hanhinen <santeri.hanhinen@gmail.com>
2025-05-05 15:25:08 +0300

    added test for viewing pokemon page

--- test/e2e-tests/playTest.spec.jsx
+++ test/e2e-tests/playTest.spec.jsx
@@ -8,4 +8,9 @@ describe('Pokedex', () => {
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
   })
+  test('pokemon page can be opened', async ({ page }) => {
+
+    await page.goto('/pokemon/ivysaur')
+    await expect(page.getByText('chlorophyll')).toBeVisible()
+  })
 })
